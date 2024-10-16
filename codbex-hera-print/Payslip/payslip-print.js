e13d2a54011f72838c639cd61e35428749ac81e8
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-10-16 14:26:08 +0300

    Add payslip print (#1)

---
+++ codbex-hera-print/Payslip/payslip-print.js
@@ -0,0 +1,15 @@
+const viewData = {
+    id: 'payslip-print',
+    label: 'Print',
+    link: '/services/web/codbex-hera-print/Payslip/print-payslip.html',
+    perspective: 'Payrolls',
+    view: 'PayrollEntry',
+    type: 'entity',
+    order: 117
+};
+
+if (typeof exports !== 'undefined') {
+    exports.getDialogWindow = function () {
+        return viewData;
+    }
+}
---
+++ codbex-hera-print/Payslip/print-payslip-controller.js
@@ -0,0 +1,18 @@
+const app = angular.module('templateApp', ['ideUI', 'ideView']);
+app.controller('templateController', ['$scope', '$http', 'ViewParameters', function ($scope, $http, ViewParameters) {
+    const params = ViewParameters.get();
+
+    const printPayslipUrl = "/services/ts/codbex-hera-print/Payslip/api/PayslipService.ts/" + params.id;
+
+    $http.get(printPayslipUrl)
+        .then(function (response) {
+            $scope.PayrollEntry = response.data.payrollEntry;
+            $scope.Date = new Date();
+            $scope.Employee = response.data.employee;
+            $scope.Department = response.data.department;
+            $scope.JobRole = response.data.jobRole;
+            $scope.Salary = response.data.salary;
+            $scope.Currency = response.data.currency;
+            $scope.Gross = $scope.PayrollEntry.NetSalary + $scope.PayrollEntry.Taxes;
+        });
+}]);
